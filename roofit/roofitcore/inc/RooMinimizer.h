c294d6fd22aaecf26f9ea6c7b654a3dbbe4eaa64
E. G. Patrick Bos <egpbos@gmail.com>
2021-07-03 21:13:47 +0200

    add RooGradMinimizerFcn
    This commit adds RooGradMinimizerFcn, an alternative to RooMinimizerFcn that calculates gradient itself outside of Minuit during minimization.
    
    To be able to use RooGradMinimizerFcn, RooMinimizer had to be refactored to be able to use RooAbsMinimizerFcn instead of the concrete RooMinimizerFcn class. To accomodate this change, several other changes had to be made:
    - Functions getMultiGenFcn, fitterFcn and fitFcn were added which are used in several places as convenience functions to access the concrete RooAbsMinimizerFcn object with its proper type. The concrete type matters, for instance, when passing the class to the derivator, because a gradient enabled *MinimizerFcn needs to take different overloads than the classic function-value-only RooMinimizerFcn. A FcnMode enum class was added for this disambiguation as well.
    - Construction of a RooMinimizer can be done the same as always when using a classic RooMinimizerFcn. However, to activate RooGradMinimizerFcn, a new create function can be used with the proper type as template parameter. In a later PR, this pattern will be extended with another gradient-enabled RooAbsMinimizerFcn instantiation that will also include multi-processing implementations.
    
    Since we had to refactor construction, we took the opportunity to also put default parameters in the header declaration, instead of in the constructor initializer lists.
    
    Finally, this commit changes the default minimizer from Minuit to Minuit2.
    
    Other than these RooMinimizer changes and the addition of RooGradMinimizerFcn, a new test was added called testRooGradMinimizer. It also uses a test_lib.h header that will also be used later on in tests of other new likelihood minimization implementations.

--- roofit/roofitcore/inc/RooMinimizer.h
+++ roofit/roofitcore/inc/RooMinimizer.h
@@ -6,6 +6,7 @@
  *   WV, Wouter Verkerke, UC Santa Barbara, verkerke@slac.stanford.edu       *
  *   DK, David Kirkby,    UC Irvine,         dkirkby@uci.edu                 *
  *   AL, Alfio Lazzaro,   INFN Milan,        alfio.lazzaro@mi.infn.it        *
+ *   PB, Patrick Bos,     NL eScience Center, p.bos@esciencecenter.nl        *
  *                                                                           *
  *                                                                           *
  * Redistribution and use in source and binary forms,                        *
@@ -16,6 +17,8 @@
 #ifndef ROO_MINIMIZER
 #define ROO_MINIMIZER
 
+#include <memory>  // shared_ptr, unique_ptr
+
 #include "TObject.h"
 #include "TStopwatch.h"
 #include <fstream>
@@ -24,8 +27,16 @@
 #include <utility>
 #include "TMatrixDSymfwd.h"
 
-#include "Fit/Fitter.h"
+#include "RooArgList.h" // cannot just use forward decl due to default argument in lastMinuitFit
+
 #include "RooMinimizerFcn.h"
+#include "RooGradMinimizerFcn.h"
+
+#include "RooSentinel.h"
+#include "RooMsgService.h"
+
+#include "Fit/Fitter.h"
+#include <stdexcept> // logic_error
 
 class RooAbsReal ;
 class RooFitResult ;
@@ -37,8 +48,11 @@ class RooPlot ;
 
 class RooMinimizer : public TObject {
 public:
+  enum class FcnMode { classic, gradient };
 
   RooMinimizer(RooAbsReal& function) ;
+  template <typename MinimizerFcn = RooMinimizerFcn>
+  static std::unique_ptr<RooMinimizer> create(RooAbsReal &function);
   virtual ~RooMinimizer() ;
 
   enum Strategy { Speed=0, Balance=1, Robustness=2 } ;
@@ -91,7 +105,10 @@ public:
 
   ROOT::Fit::Fitter* fitter() ;
   const ROOT::Fit::Fitter* fitter() const ;
-  
+
+  ROOT::Math::IMultiGenFunction* getFitterMultiGenFcn() const;
+  ROOT::Math::IMultiGenFunction* getMultiGenFcn() const;
+
 protected:
 
   friend class RooAbsPdf ;
@@ -100,37 +117,103 @@ protected:
   void profileStart() ;
   void profileStop() ;
 
-  inline Int_t getNPar() const { return fitterFcn()->NDim() ; }
+  inline Int_t getNPar() const { return fitterFcn()->getNDim() ; }
   inline std::ofstream* logfile() { return fitterFcn()->GetLogFile(); }
   inline Double_t& maxFCN() { return fitterFcn()->GetMaxFCN() ; }
-  
-  const RooMinimizerFcn* fitterFcn() const {  return ( fitter()->GetFCN() ? ((RooMinimizerFcn*) fitter()->GetFCN()) : _fcn ) ; }
-  RooMinimizerFcn* fitterFcn() { return ( fitter()->GetFCN() ? ((RooMinimizerFcn*) fitter()->GetFCN()) : _fcn ) ; }
+
+  const RooAbsMinimizerFcn *fitterFcn() const;
+  RooAbsMinimizerFcn *fitterFcn();
+
+  bool fitFcn() const;
 
 private:
+  template <typename MinimizerFcn = RooMinimizerFcn>
+  RooMinimizer(RooAbsReal &function, MinimizerFcn* /* used only for template deduction */);
 
-  Int_t       _printLevel ;
-  Int_t       _status ;
-  Bool_t      _profile ;
+  Int_t _printLevel = 1;
+  Int_t _status = -99;
+  Bool_t _profile = kFALSE;
 
-  Bool_t      _verbose ;
-  TStopwatch  _timer ;
-  TStopwatch  _cumulTimer ;
-  Bool_t      _profileStart ;
+  Bool_t _verbose = kFALSE;
+  TStopwatch _timer;
+  TStopwatch _cumulTimer;
+  Bool_t _profileStart = kFALSE;
 
-  TMatrixDSym* _extV ;
+  TMatrixDSym *_extV = 0;
 
-  RooMinimizerFcn *_fcn;
-  std::string _minimizerType;
+  RooAbsMinimizerFcn *_fcn;
+  std::string _minimizerType = "Minuit2";
+  FcnMode _fcnMode;
 
   static ROOT::Fit::Fitter *_theFitter ;
 
   std::vector<std::pair<std::string,int> > _statusHistory ;
 
   RooMinimizer(const RooMinimizer&) ;
-	
+
   ClassDef(RooMinimizer,1) // RooFit interface to ROOT::Fit::Fitter
 } ;
 
+////////////////////////////////////////////////////////////////////////////////
+/// Construct MINUIT interface to given function. Function can be anything,
+/// but is typically a -log(likelihood) implemented by RooNLLVar or a chi^2
+/// (implemented by RooChi2Var). Other frequent use cases are a RooAddition
+/// of a RooNLLVar plus a penalty or constraint term. This class propagates
+/// all RooFit information (floating parameters, their values and errors)
+/// to MINUIT before each MINUIT call and propagates all MINUIT information
+/// back to the RooFit object at the end of each call (updated parameter
+/// values, their (asymmetric errors) etc. The default MINUIT error level
+/// for HESSE and MINOS error analysis is taken from the defaultErrorLevel()
+/// value of the input function.
+
+template <typename MinimizerFcn>
+RooMinimizer::RooMinimizer(RooAbsReal &function, MinimizerFcn* /* value unused */)
+{
+   RooSentinel::activate();
+
+   if (_theFitter)
+      delete _theFitter;
+   _theFitter = new ROOT::Fit::Fitter;
+   _theFitter->Config().SetMinimizer(_minimizerType.c_str());
+   setEps(1.0); // default tolerance
+
+   _fcn = new MinimizerFcn(&function, this, _verbose);
+
+   // make sure to order correctly so that child classes get checked for first TODO
+   if (dynamic_cast<RooGradMinimizerFcn *>(_fcn)) {
+      _fcnMode = FcnMode::gradient;
+   } else if (dynamic_cast<RooMinimizerFcn *>(_fcn)) {
+      _fcnMode = FcnMode::classic;
+   } else {
+      throw std::logic_error("RooMinimizer's MinimizerFcn template argument must be (a subclass of) RooMinimizerFcn or "
+                             "RooGradMinimizerFcn. MinuitFcnGrad is used in the other RooMinimizer ctor.");
+   }
+
+   // default max number of calls
+   _theFitter->Config().MinimizerOptions().SetMaxIterations(500 * _fcn->getNDim());
+   _theFitter->Config().MinimizerOptions().SetMaxFunctionCalls(500 * _fcn->getNDim());
+
+   // Shut up for now
+   setPrintLevel(-1);
+
+   // Use +0.5 for 1-sigma errors
+   setErrorLevel(function.defaultErrorLevel());
+
+   // Declare our parameters to MINUIT
+   _fcn->Synchronize(_theFitter->Config().ParamsSettings(), _fcn->getOptConst(), _verbose);
+
+   // Now set default verbosity
+   if (RooMsgService::instance().silentMode()) {
+      setPrintLevel(-1);
+   } else {
+      setPrintLevel(1);
+   }
+}
+
+// static function
+template <typename MinimizerFcn>
+std::unique_ptr<RooMinimizer> RooMinimizer::create(RooAbsReal &function) {
+   return std::unique_ptr<RooMinimizer>(new RooMinimizer(function, static_cast<MinimizerFcn*>(nullptr)));
+}
 
 #endif
