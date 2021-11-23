d2c289524bef735aa53906b955766d2b65005777
E. G. Patrick Bos <egpbos@gmail.com>
2021-11-23 21:18:50 +0100

    zmq builtin: tweak Findcppzmq module
    Successfully tested on CentOS 8.

--- cmake/modules/Findcppzmq.cmake
+++ cmake/modules/Findcppzmq.cmake
@@ -1,22 +1,21 @@
-if (NOT ZeroMQ_FOUND)
+if (NOT (ZeroMQ_FOUND OR TARGET libzmq))
     message(FATAL_ERROR "Search for libzmq first!")
-endif ()
+endif()
 
-find_path(CPPZMQ_INCLUDE_DIRS "zmq.hpp"
+find_path(cppzmq_INCLUDE_DIRS "zmq.hpp"
         HINTS "${ZeroMQ_INCLUDE_DIR}"
-        PATHS "${CPPZMQ_DIR}" "$ENV{CPPZMQ_DIR}"
         PATH_SUFFIXES "include" "cppzmq"
         )
-mark_as_advanced (CPPZMQ_INCLUDE_DIRS)
+mark_as_advanced(cppzmq_INCLUDE_DIRS)
 
-if (CPPZMQ_INCLUDE_DIRS)
-    add_library (cppzmq INTERFACE IMPORTED)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args (cppzmq DEFAULT_MSG cppzmq_INCLUDE_DIRS)
+
+if(cppzmq_FOUND)
+    add_library(cppzmq INTERFACE IMPORTED)
     set_target_properties(cppzmq PROPERTIES
             INTERFACE_INCLUDE_DIRECTORIES "${CPPZMQ_INCLUDE_DIRS}"
             INTERFACE_LINK_LIBRARIES libzmq
             )
-    set (CPPZMQ_LIBRARIES cppzmq)
-endif ()
-
-include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args (CPPZMQ DEFAULT_MSG CPPZMQ_INCLUDE_DIRS)
+    set(CPPZMQ_LIBRARIES cppzmq)
+endif()
